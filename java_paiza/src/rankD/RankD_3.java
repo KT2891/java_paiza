61dde460d1fe90d5350954ac3463c9a530936e0f
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-02-07 15:13:57 +0900

    標準出力練習

---
+++ java_paiza/src/rankD/RankD_3.java
@@ -0,0 +1,12 @@
+package rankD;
+
+public class RankD_3 {
+
+	public static void main(String[] args) {
+		String word = "He likes paiza";
+		String[] a = word.split(" ");
+		for (String t: a)
+			System.out.println(t);
+ 	}
+
+}
---
+++ java_paiza/src/rankD/RankD_4.java
@@ -0,0 +1,12 @@
+package rankD;
+
+public class RankD_4 {
+
+	public static void main(String[] args) {
+		String word = "one two three four five";
+		String words[] = word.split(" ");
+		for(String t: words)
+			System.out.println(t);
+	}
+
+}
