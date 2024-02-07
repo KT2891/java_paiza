153cf412d82ad40fe71f20cf85a98a817c734196
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-02-07 15:26:31 +0900

    整数の入出力

---
+++ java_paiza/src/rankD/RankD_6.java
@@ -0,0 +1,17 @@
+package rankD;
+
+import java.util.Scanner;
+
+public class RankD_6 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		for(int i = 0; i < 1000; i++) {
+			int num = Integer.parseInt(sc.next());
+			System.out.println(num);
+		}
+		
+		sc.close();
+	}
+}
