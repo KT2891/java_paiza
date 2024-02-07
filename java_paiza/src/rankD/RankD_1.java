99104473b6b3ef51f2154b3c48ff3a428ef72b8e
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-02-07 14:42:59 +0900

    フォルダ名変更

--- java_paiza/src/rankD/RankD_1.java
+++ java_paiza/src/rankD/RankD_1.java
@@ -1,16 +1,18 @@
-package rankC;
+package rankD;
 
 import java.util.Scanner;
 
 public class RankD_1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		int wordAmount = 10;
 		String words[] = new String[wordAmount];
 		for (int i = 0; i < 10; i++) {
 			words[i] = sc.next();
 			System.out.println(words[i]);
 		}
+		sc.close();
 	}
 }
 	
