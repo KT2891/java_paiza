fe9cd4c28df669f87c17c043ad70ed025cbc14c9
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-02-07 14:38:57 +0900

    paizaをjavaにて勉強開始

---
+++ java_paiza/src/rankC/RankD_1.java
@@ -0,0 +1,16 @@
+package rankC;
+
+import java.util.Scanner;
+
+public class RankD_1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int wordAmount = 10;
+		String words[] = new String[wordAmount];
+		for (int i = 0; i < 10; i++) {
+			words[i] = sc.next();
+			System.out.println(words[i]);
+		}
+	}
+}
+	
