d0a1c7d13cb253574a63dd9fe75ccec5ef5473de
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-02-20 15:33:41 +0900

    編集

--- java_paiza/src/rankC/RankC_07.java
+++ java_paiza/src/rankC/RankC_07.java
@@ -4,23 +4,32 @@ import java.util.Scanner;
 
 public class RankC_07 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int boxArgsCount = 3;
-		
-		int boxCount = sc.nextInt();
-		int ballDiameter = sc.nextInt() * 2;
-
-		int[] boxData = new int[boxArgsCount];
-		
-		for(int i = 0; i < boxCount; i++) {
-			for(int j = 0; j < boxArgsCount; j++) boxData[j] = sc.nextInt();
-			if(boxData[0] >= ballDiameter && boxData[1] >= ballDiameter && boxData[2] >= ballDiameter)
-				System.out.println(i + 1);
-		}
-		
-		sc.close();
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int dimensionsPerBox = 3; // 名前をより明確に
+        
+        try {
+            int boxCount = sc.nextInt();
+            int ballDiameter = sc.nextInt() * 2;
+    
+            int[] boxData = new int[dimensionsPerBox];
+            
+            for(int i = 0; i < boxCount; i++) {
+                for(int j = 0; j < dimensionsPerBox; j++) {
+                    boxData[j] = sc.nextInt();
+                }
+                if(checkBoxSize(boxData, ballDiameter))
+                    System.out.println(i + 1);
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter integers only.");
+        } finally {
+            sc.close();
+        }
+    }
+    
+    private static boolean checkBoxSize(int[] boxData, int ballDiameter) {
+        return boxData[0] >= ballDiameter && boxData[1] >= ballDiameter && boxData[2] >= ballDiameter;
+    }
 }
